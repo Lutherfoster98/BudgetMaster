6fb317ba86ec197ea92f161b56fc1cde139dad4f
Stephanie-Renee-Escue <sescue@my.apsu.edu>
2020-05-05 17:32:39 -0500

    Adding layout files

--- app/src/main/java/com/joshuafoster/budgetmaster/MainActivity.java
+++ app/src/main/java/com/joshuafoster/budgetmaster/MainActivity.java
@@ -2,10 +2,18 @@ package com.joshuafoster.budgetmaster;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -50,7 +58,60 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.editSavingsBtn:
                 setContentView(R.layout.savings_overview);
                 break;
+
+            // Buttons on home page
+            case R.id.createNewBudgetButton:
+
+                break;
+            case R.id.viewBudgetButton:
+
+                break;
+            case R.id.plusSignImageView:
+                //setContentView(R.layout.new_income);
+                break;
+            case R.id.minusSignImageView:
+                //setContentView(R.layout.new_expense);
+                break;
         }
 
     }
+
+    /*class displayCalendar() {
+        final TextView startDate, endDate;
+        DatePickerDialog.OnDateSetListener DateSetListener = null;
+
+            startDate = findViewById(R.id.startDateET);
+            endDate = findViewById(R.id.endDateET);
+
+            startDate.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Calendar cal = Calendar.getInstance();
+                    int year = cal.get(Calendar.YEAR);
+                    int month = cal.get(Calendar.MONTH);
+                    int day = cal.get(Calendar.DAY_OF_MONTH);
+
+                    DatePickerDialog dialog = new DatePickerDialog(
+                            MainActivity.this,
+                            android.R.style.Theme_Holo_Light_Dialog_MinWidth,
+                            DateSetListener,
+                            year,month,day);
+                    dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                    dialog.show();
+                }
+            });
+
+            DateSetListener = new DatePickerDialog.OnDateSetListener() {
+                @Override
+                public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                    month = month + 1;
+                    Log.d("DATE", "onDateSet: mm/dd/yyy: " + month + "/" + day + "/" + year);
+
+                    String date = month + "/" + day + "/" + year;
+                    startDate.setText(date);
+                }
+            };
+        }
+    }
+    } */
 }
